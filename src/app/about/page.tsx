613a641e6d23618098fe2843ba2955910d413784
Sergio Arce <arcesech30@gmail.com>
2025-02-12 18:39:53 +0100

    add dynamic render use cookies in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+  const cookiesStore = await cookies()
+  const theme = cookiesStore.get('theme')
+  console.log({ theme })
   console.log("About server component")
-  return <div>AboutPage</div>;
+  return <div>AboutPage {new Date().toLocaleDateString()}</div>;
 }
