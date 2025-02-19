dafa0b37037fa9392a89c9ca805171e4dea34d95
Sergio Arce <arcesech30@gmail.com>
2025-02-19 16:43:38 +0100

    add context provider in cliente-route

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -23,30 +23,36 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoutePage() {
 
   const settings = {
     dots: true
   }
+
+  const theme = useTheme()
  
   return (
-    
-    <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src="https://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="https://picsum.photos/400/200" />
-        </div>
-      </Slider>
-    </div>
+    <>
+      <h1 style={{ color: theme.colors.primary }}>Client component</h1>
+
+      <div className="image-slider-container">
+        <Slider {...settings}>
+          <div>
+            <img src="https://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="https://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="https://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="https://picsum.photos/400/200" />
+          </div>
+        </Slider>
+      </div>
+    </>
   );
 }
