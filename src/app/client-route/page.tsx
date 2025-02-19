49675bd0323eb291e1efdc5001d29b399a90f49e
Sergio Arce <arcesech30@gmail.com>
2025-02-19 17:05:40 +0100

    add exclusive code for the client component

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -24,6 +24,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoutePage() {
 
@@ -32,10 +33,12 @@ export default function ClientRoutePage() {
   }
 
   const theme = useTheme()
+
+  const clienteResult = clientSideFunction()
  
   return (
     <>
-      <h1 style={{ color: theme.colors.primary }}>Client component</h1>
+      <h1 style={{ color: theme.colors.primary }}>Client component { clienteResult }</h1>
 
       <div className="image-slider-container">
         <Slider {...settings}>
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,12 +1,15 @@
 import { serverSideFunction } from "@/utils/server-utils";
 import { ImageSlider } from "@/components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ServerRoutePage() {
 
   const result = serverSideFunction()
 
+  const clienteResult = clientSideFunction()
+
   return <>
-    <div>Server Route Page { result }</div>
+    <div>Server Route Page { result } { clienteResult }</div>
     <ImageSlider /> 
   </>
 }
